81013a4116306241f18df5cb93725434552f2fa2
ddorrddi <lhhleh@naver.com>
2026-03-09 02:29:43 +0900

    feat: 20260304

---
+++ 20260304/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello C++!" << std::endl;
+    std::cout << "This program ends" << std::endl;
+    return 0;
+}
+
+/*
+cpp프로그램은 main()에서 시작
+표준 출력은 std::out을 사용
+줄바꿈은 '\n'이나 std::endl을 사용
+return 0;은 프로그램 정상 종료의 의미
+*/
